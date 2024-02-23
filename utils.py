94cee88ee572c66015a5825347b6118e5307d619
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-02-23 17:43:10 +0100

    refactor: Format files to match pycodestyle

--- utils.py
+++ utils.py
@@ -1,18 +1,18 @@
 """ This module will contain util functions
 that will be used in the program"""
-import os
 from pathlib import Path
 
+
 def list_songs(folders: list) -> None:
-	"""Function to accepts list
-	of directories to check for songs
-	and list them out"""
-	mp3_total_files = []
-	for folder in folders:
-		# Deal with cases where folder does not exist.
-		folder_path = Path(folder)
-		if folder_path.exists() and folder_path.is_dir():
-			mp3_files = folder_path.glob("*.mp3")
-			mp3_total_files.extend([str(mp3) for mp3 in mp3_files])
-	for idx, mp3 in enumerate(mp3_total_files):
-		print(idx, mp3)
+    """Function to accepts list
+    of directories to check for songs
+    and list them out"""
+    mp3_total_files = []
+    for folder in folders:
+        # Deal with cases where folder does not exist.
+        folder_path = Path(folder)
+        if folder_path.exists() and folder_path.is_dir():
+            mp3_files = folder_path.glob("*.mp3")
+            mp3_total_files.extend([str(mp3) for mp3 in mp3_files])
+    for idx, mp3 in enumerate(mp3_total_files):
+        print(idx, mp3)
