a1c0f47e599edd7e8476d0c53e881dd3f3ca9f4e
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-02-23 19:59:07 +0100

    feat: Allow music accept path optionally inplace of string

--- library/music.py
+++ library/music.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from pygame import mixer
 from mutagen import _util, _file
 
@@ -6,7 +7,7 @@ class Music(mixer.Sound):
     channel: mixer.Channel
     metadata: _file.FileType
 
-    def __init__(self, file: str) -> None:
+    def __init__(self, file: str | Path) -> None:
         super().__init__(file)
 
         try:
