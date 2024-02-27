f76c92b612e395967fc417e81a2217166acc66d7
deelight <akingbenidavidoyin@gmail.com>
2024-02-27 11:53:41 +0100

    Fix: After pause and resuming, the next song after the current song plays from the timestamp given by previous pause command --- Now fixed

--- _music_script.py
+++ _music_script.py
@@ -11,7 +11,7 @@ pygame.init()
 current_playing = 0
 
 
-def play(now_playin: int = 0):
+def play(now_playin: int = 0, pos: int = pos):
     global current_playing
     opts = argv[3:]
 
@@ -34,7 +34,7 @@ def play(now_playin: int = 0):
             os.close(fd1)
         current_playing = now_playin
         pass
-    play(now_playin + 1)
+    play(now_playin + 1, pos=0)
 
 
 if cmd == Cmd.PLAY:
