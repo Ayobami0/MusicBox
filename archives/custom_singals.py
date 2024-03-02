d383f26ae47d46796e0e04b58edb585ffaf53b72
deelight <akingbenidavidoyin@gmail.com>
2024-03-02 13:09:37 +0100

    Full play implementation

--- archives/custom_singals.py
+++ archives/custom_singals.py
@@ -1,8 +1,12 @@
-import signal
+# import eyed3
 
+# def is_mp3_valid(file_path):
+#     try:
+#         _ = eyed3.load(file_path)
+#         return True
+#     except:
+#         return False
 
-class PauseSignal(signal.Signals):
-    pass
-
-class ResumeSignal(signal.Signals):
-    pass
+# # Usage
+# mp3_file_path = "path/to/your.mp3"
+# print(f"Is the file valid MP3? {is_mp3_valid(mp3_file_path)}")
